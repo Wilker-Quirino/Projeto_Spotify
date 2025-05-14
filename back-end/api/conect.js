6475717fd2207f5373741d3e6f489136ed8ffce1
Wilker Quirino <wilkerquirino18@hotmail.com>
2025-05-14 16:02:13 -0400

    atualizando

--- back-end/api/conect.js
+++ back-end/api/conect.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://admin:WqS319847021994@cluster0.628mtkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://admin:adminRealadmin@cluster0.628mtkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
